289263ee1fd50e97e966f5687db93479851d7eb4
Harry Sun <zoomhgtk@gmail.com>
2016-08-18 16:20:58 +0800

    make it on same line, deleted pprint

--- set_srx_rules.py
+++ set_srx_rules.py
@@ -1,14 +1,15 @@
-import pprint as pp
 import socket
 
 address_set_name = raw_input("please enter name of address-set >")
 
 def set_address(hostname):
 	ip = socket.gethostbyname(hostname)
-	print "set security zones security-zone Transit address-book address %s %s/32" % (hostname, ip)
+	print "set security zones security-zone",
+	print "Transit address-book address %s %s/32" % (hostname, ip)
 
 def set_address_book(hostname):
-	print "set security zones security-zone Transit address-book address-set %s address %s" % (address_set_name, hostname)
+	print "set security zones security-zone",
+	print "Transit address-book address-set %s address %s" % (address_set_name, hostname)
 
 for i in range(1,24):
 	host = "dn%d.phx2.cbsig.net" % i
