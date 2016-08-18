ed2641ebceea3c052585b943404c9a819483f854
Harry Sun <zoomhgtk@gmail.com>
2016-08-18 13:52:45 +0800

    done

--- set_srx_rules.py
+++ set_srx_rules.py
@@ -1,12 +1,17 @@
 import pprint as pp
 import socket
+
+address_set_name = raw_input("please enter name of address-set >")
+
 def set_address(hostname):
 	ip = socket.gethostbyname(hostname)
-	print "set address %s %s/32" % (hostname, ip)
+	print "set security zones security-zone Transit address-book address %s %s/32" % (hostname, ip)
+
+def set_address_book(hostname):
+	print "set security zones security-zone Transit address-book address-set %s address %s" % (address_set_name, hostname)
 
 for i in range(1,24):
 	host = "dn%d.phx2.cbsig.net" % i
 	set_address(host)
-
-def set_address_book(hostname):
+	set_address_book(host)
 
